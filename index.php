0d0f7b30a5bc62f751f1150937ae497cefd271ac
Yoan Le Tiec <yoan.letiec@icloud.com>
2019-06-19 18:03:27 +0200

    J'ai ajouté un titre

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exercice OpenClassrooms</title>
+</head>
+<body>
+	<h1>Si tu corriges mon devoir, viens vite découvrir www.linkybag.com ;-)</h1>
+</body>
+</html>
