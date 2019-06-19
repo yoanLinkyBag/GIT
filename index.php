1767bf80e6d0c1dd3abf20ddbd93226ee925547b
Yoan Le Tiec <yoan.letiec@icloud.com>
2019-06-19 19:18:29 +0200

    Ajout d'un très court texte

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
 	<h1>Si tu corriges mon devoir, viens vite découvrir www.linkybag.com ;-)</h1>
+	<p>Encore une petite modification, et ça devrait le faire normalement</p>
 </body>
 </html>
