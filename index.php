8e02a17c6d7889916f04416e7789bc6376777d4f
Yoan Le Tiec <yoan.letiec@icloud.com>
2019-06-20 18:26:31 +0200

    Création d'un conflit

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 </head>
 <body>
 	<h1>Si tu corriges mon devoir, viens vite découvrir www.linkybag.com ;-)</h1>
-	<p>Encore une petite modification, et ça devrait le faire</p>
+	<p>Je viens de changer cette ligne pour créer un conflit</p>
 </body>
 </html>
